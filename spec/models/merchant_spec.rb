00b028485c377ddf2597a7f9482aba5a14994b31
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-08 20:11:56 -0600

    add coupons relationship and fix RSpec.describe

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Merchant do
+RSpec.describe Merchant, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
@@ -10,6 +10,7 @@ describe Merchant do
     it {should have_many(:invoices).through(:invoice_items)}
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:coupons) }
 
   end
 
