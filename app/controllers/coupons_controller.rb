255df25c950a9c1a39b491498ea5d39a4bb591a7
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-10 17:18:21 -0600

    add new create and coupon_params

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,4 +3,26 @@ class CouponsController < ApplicationController
     @coupons = Coupon.includes(:merchant)
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = @merchant.coupons.build
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id]) 
+    @coupon = @merchant.coupons.new(coupon_params)
+
+    if @coupon.save
+      redirect_to merchant_coupons_path(@merchant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :value, :amount_type)
+  end
 end
