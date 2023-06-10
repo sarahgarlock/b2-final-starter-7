e38183194b0623a4aac366db387d133c860b28bd
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-10 17:21:03 -0600

    re add coupon table with integer for amount

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_172705) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_225126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_172705) do
     t.string "name"
     t.string "code"
     t.integer "value"
-    t.integer "status", default: 0
+    t.integer "amount_type"
+    t.integer "status"
     t.bigint "merchant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
