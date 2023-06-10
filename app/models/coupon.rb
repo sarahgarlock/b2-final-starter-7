ca7a40099c3fab4a5248c211e0cb221d252095a1
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-10 17:18:57 -0600

    add enums to coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,7 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
-  has_many :invoices, through: :merchant
+  has_many :invoices
+
+  enum status: [:active, :inactive]
+  enum amount_type: [:percent, :dollar]
 end
