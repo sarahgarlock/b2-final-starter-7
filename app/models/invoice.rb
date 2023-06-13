bf9176a48c0683587e97ef682a553c4258262166
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-13 12:16:04 -0600

    update discount method to not accept negative numbers

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -17,12 +17,15 @@ class Invoice < ApplicationRecord
 
   def total_revenue_after_discount
     total_rev = invoice_items.sum("unit_price * quantity")
-    if coupon.present?
-      if coupon.amount_type == "percent"
-        total_rev - (total_rev * (coupon.value.to_f / 100))
-      else
-        total_rev - coupon.value
-      end
+    return total_rev unless coupon.present?
+  
+    if coupon.amount_type == "percent"
+      total_rev -= total_rev * (coupon.value.to_f / 100)
+    else
+      new_total_rev = total_rev - coupon.value
+      new_total_rev = 0 if new_total_rev < 0
+      total_rev = new_total_rev
     end
+    total_rev
   end
 end
